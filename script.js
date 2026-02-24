25ce59d0b374a88437f78274b5b8d97f0e7d90bf
ibrahimmazumder15 <ibrahimmazumder17@gmail.com>
2026-02-24 18:51:19 +0600

    complete calculatecount and togglestyle and push cardInfo function

--- script.js
+++ script.js
@@ -1,4 +1,63 @@
+let interviewList = [];
+let rejectedList = [];
+
 let total = document.getElementById('total');
 let  interview = document.getElementById('interview');
 let rejected = document.getElementById('rejected');
 
+const allFilterBtn = document.getElementById('all-btn')
+const interviewFilterBtn = document.getElementById('interview-btn')
+const rejectedFilterBtn = document.getElementById('rejected-btn')
+
+
+const jobsCardSection = document.getElementById('jobsCard');
+console.log(jobsCardSection.children.length); 
+
+const mainContainer = document.querySelector('main')
+console.log(mainContainer)
+
+function calculateCount (){
+     total.innerText = jobsCardSection.children.length
+     interview.innerText = interviewList.length
+     rejected.innerText = rejectedList.length
+}
+
+calculateCount()
+
+function toggleStyle(id){
+     console.log("click",id);
+     allFilterBtn.classList.remove('bg-[#3B82F6]');
+     interviewFilterBtn.classList.remove('bg-[#3B82F6]');
+     rejectedFilterBtn.classList.remove('bg-[#3B82F6]');
+
+     allFilterBtn.classList.add('bg-base-100');
+     interviewFilterBtn.classList.add('bg-base-100');
+     rejectedFilterBtn.classList.add('bg-base-100');
+
+     const selected = document.getElementById(id);
+     console.log(selected);
+     selected.classList.add('bg-[#3B82F6]')
+}
+
+mainContainer.addEventListener('click', function(even){
+   const parentNode = even.target.parentNode.parentNode;
+   const jobsName = parentNode.querySelector('.badge').innerText
+   
+   const cardInfo ={
+     jobsName,
+   }
+   
+
+   const jobExist = interviewList.find(item=> item.jobsName == cardInfo.jobsName)
+
+   if(!jobExist){
+     interviewList.push(cardInfo);
+   }
+   
+console.log(interviewList)
+
+
+   
+
+   
+});
